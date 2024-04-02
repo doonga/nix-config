b8cf2ffe2ad65a868e26d7d8e0c813e59fcfc593
Todd Punderson <agile.car7544@undercovermail.org>
2024-04-02 18:27:14 -0400

    feat: Re-add nix-build workflow

--- hosts/_modules/common/nix.nix
+++ hosts/_modules/common/nix.nix
@@ -10,12 +10,14 @@
         "https://nix-community.cachix.org"
         "https://cache.garnix.io"
         "https://numtide.cachix.org"
+        "https://greyrock.cachix.org"
       ];
 
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+        "greyrock.cachix.org-1:n1HVJSHb4x0qisXTDIUeKnfQhw6CngYNDEzLF0IWc8Y="
       ];
 
       # Fallback quickly if substituters are not available.
