dad03be101755c95640d886824172120e125cb08
Todd Punderson <agile.car7544@undercovermail.org>
2024-05-02 11:03:35 -0400

    feat(nas): Network changes

--- hosts/nas/hardware-configuration.nix
+++ hosts/nas/hardware-configuration.nix
@@ -31,9 +31,15 @@
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
+  # networking.useDHCP = lib.mkDefault true;
+  networking.interfaces.eno1.useDHCP = lib.mkDefault true;
+  networking.interfaces.enp1s0f0 = {
+    ipv4.addresses = [{
+      address = "10.1.9.3";
+      prefixLength = 28;
+    }];
+    mtu = 9000;
+  };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
